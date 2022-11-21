5ec466dfea2604e9adb66afcc52fb97e1d5efa26
theodore cleveland <ted.cleveland@gmail.com>
2022-11-20 18:52:36 -0600

    initial commit

---
+++ swmmbyexample/_build/jupyter_execute/examples/chapter1/introduction.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+# 
+# ## How to Use This Book
+# 
+# This book is a collection of worked SWMM examples from a variety of sources, some real, most are pedagogical.  Most examples will have a set of files needed to replicate the example, you simply have to download the files, and run the SWMM model using the various files to replicate the examples.  Alternatively you can run the models on the FreeSWMM web interface.
+# 
+# ## FreeSWMM Web 
+# 
+# The FreeSWMM Web interface is simply a VPS hosted on AWS that is accessed using a web browser.  The browser will connect to a desktop (XFCE) environment which you can use to open SWMM and the associated files. Please remember to disconnect when you are done, and be aware the system is restarted from time to time so ny files you save or damage for that matter are lost.
+# 
+# [FreeSWMM (via NoVNC)](http://18.210.253.169:6081/vnc.html?host=18.210.253.169&port=6081)
+
+# In[ ]:
+
+
+
+
